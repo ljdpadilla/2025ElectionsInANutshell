b5ef3b9f858da4dce3b60e0f3361b60c3689549d
geraldine-sudo <gabadiola@up.edu.ph>
2025-11-08 11:40:19 +0800

    aboutuscolor

--- site/src/routes/aboutus/+page.svelte
+++ site/src/routes/aboutus/+page.svelte
@@ -7,48 +7,13 @@
     let { data }: PageProps = $props();
 </script>
 
-<section class="mt-10 mb-30 flex flex-col gap-6 items-center align-middle justify-center">
-    <h1 class="text-7xl font-bold text-center ">About Us</h1>
-    <Button
-        href="{base}/"
-        size="sm"
-        variant="secondary"
-    >
-        Go Back Home
-    </Button>
-
-</section>
-<div class="about-box">
-  <img src="/lance.jpg" alt="Lance Joseph D. Padilla" class="profile-pic" />
-  <div class="about-text">
-    <h2>Lance Joseph D. Padilla</h2>
-    <p>Team Developer</p>
-  </div>
-</div>
-
-<div class="about-box">
-  <img src="/stephen.jpg" alt="Lance Joseph D. Padilla" class="profile-pic" />
-  <div class="about-text">
-    <h2>Lance Joseph D. Padilla</h2>
-    <p>Team Developer</p>
-  </div>
-</div>
-
-<div class="about-box">
-  <img src="/geraldine.jpg" alt="Lance Joseph D. Padilla" class="profile-pic" />
-  <div class="about-text">
-    <h2>Lance Joseph D. Padilla</h2>
-    <p>Team Developer</p>
-  </div>
-</div>
-
 <style>
   .about-box {
     display: flex;
     align-items: center;           
     justify-content: space-between;
     width: 1000px;
-    background-color: grey;
+    background-color: rgb(53, 50, 50);
     border-radius: 15px;
     padding: 20px;
     margin: 20px auto;           
@@ -83,5 +48,40 @@
     flex-shrink: 0;         
   }
 </style>
+
+<section class="mt-10 mb-30 flex flex-col gap-6 items-center align-middle justify-center">
+    <h1 class="text-7xl font-bold text-center ">About Us</h1>
+    <Button
+        href="{base}/"
+        size="sm"
+        variant="secondary"
+    >
+        Go Back Home
+    </Button>
+
+</section>
+<div class="about-box">
+  <img src="/lance.jpg" alt="Lance Joseph D. Padilla" class="profile-pic" />
+  <div class="about-text">
+    <h2>Lance Joseph D. Padilla</h2>
+    <p>Team Developer</p>
+  </div>
+</div>
+
+<div class="about-box">
+  <img src="/stephen.jpg" alt="Stephen James S. Gonda" class="profile-pic" />
+  <div class="about-text">
+    <h2>Stephen James S. Gonda</h2>
+    <p>Team Developer</p>
+  </div>
+</div>
+
+<div class="about-box">
+  <img src="/geraldine.jpg" alt="Geraldine Abigail A. Badiola" class="profile-pic" />
+  <div class="about-text">
+    <h2>Geraldine Abigail A. Badiola</h2>
+    <p>Team Developer</p>
+  </div>
+</div>
 
 <Particles className="absolute inset-0 -z-10" refresh={true} />
